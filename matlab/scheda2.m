49eecda1e90e270de92105e6206b4b1971a0e317
0xMere <merenda.s.mattia@gmail.com>
2023-03-28 16:38:05 +0200

    Modifica scheda 2 @merendamattia

--- matlab/scheda2.m
+++ matlab/scheda2.m
@@ -7,7 +7,7 @@ y1 = x .* (sqrt( x.^2 + 1 ) - x );
 y2 = x .* sqrt( x.^2 + 1 ) - x.^2 ;
 y3 = x ./ (sqrt( x.^2 + 1 ) + x );
 z = [y1; y2; y3]';
-
+% ------------------------------------------
 
 % Es. 5
 clear
@@ -23,7 +23,7 @@ A(6, 1:9) = tmp;
 tmp = A(1:9, 1);
 A(1:9, 1) = A(1:9, 4);
 A(1:9, 4) = tmp;
-
+% ------------------------------------------
 
 % Es. 6
 clear
@@ -43,7 +43,7 @@ A([2 3 3]) % stampa gli elementi di riga 2 e 3 della colonna 1, da evitare crea 
 A(3,2) = A(1,1); % copia il valore di A(1,1) in A(3,2)
 A(1:2,4) = zeros (2,1) % imposta il valore 0 nelle celle di riga 1 e 2 di colonna 4
 A(2,:) = A(2,:) - A(2,1) / A(1,1) * A(1,:) % imposta un nuovo valore per tutta la riga 2
-
+% ------------------------------------------
 
 % Es. 7 
 clear 
@@ -61,3 +61,7 @@ I = tril(A);
 
 Z = zero(8,1);
 % da terminare
+
+
+
+% ------------------------------------------
