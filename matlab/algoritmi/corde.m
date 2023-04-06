e8f48bae6974db62d586a63b5acff938aaa958ce
0xMere <merenda.s.mattia@gmail.com>
2023-04-06 17:07:23 +0200

    Modifica metodo delle corde @merendamattia

--- matlab/algoritmi/corde.m
+++ matlab/algoritmi/corde.m
@@ -1,32 +1,54 @@
-function [x, resVect, nIter] = corde(f, a, b, x0, t, maxIter, print)
+function [x, iter] = metodo_corde(f, x0, x1, tol, max_iter)
+    % Calcola l'approssimazione della radice della funzione f(x) mediante il metodo delle corde
+    %
+    % Input:
+    %   - f: la funzione di cui si cerca la radice
+    %   - x0, x1: i due punti di partenza per l'interpolazione lineare
+    %   - tol: la tolleranza dell'errore assoluto per la convergenza
+    %   - max_iter: il numero massimo di iterazioni consentite
+    %
+    % Output:
+    %   - x: l'approssimazione della radice
+    %   - iter: il numero di iterazioni effettuate
+    %
     
-    %{
-    - f: funzione
-    - a: inizio intervallo
-    - b: fine intervallo
-    - x0: punto di innesco dell'iterazione
-    - t: tolleranza
-    - maxIter: numero massimo di iterazioni
-    - x: x approssimata
-    - resVect: vettore dei risultati
-    - nIter: numero dell'iterazione
-    - print: flag per fprintf
-    %}
-   
+    % Inizializzazione delle variabili
+    iter = 0;
     x = x0;
-    nIter = 0;
-    resVect = [x];
     
-    while(nIter < maxIter && abs(f(x)) > t)
-        if(print > 0)
-            fprintf("\nInterazione numero: %i, valore di x: %e", nIter, x);
+    % Calcolo dei valori iniziali della funzione nei due punti di partenza
+    f0 = f(x0);
+    f1 = f(x1);
+    
+    % Ciclo principale dell'algoritmo
+    while (iter < max_iter)
+        
+        % Calcolo dell'intersezione della retta con l'asse delle ascisse
+        x = x0 - f0/(f1 - f0) * (x1 - x0);
+        
+        % Calcolo del valore della funzione nel nuovo punto
+        fx = f(x);
+        
+        % Verifica della condizione di convergenza
+        if (abs(fx) < tol)
+            return;
         end
-        nIter = nIter + 1;
-        x = x - (b-a)/(f(b)-f(a))*f(x);
-        resVect = [resVect; x];
+        
+        % Aggiornamento dei valori dei punti di partenza
+        if (fx * f0 < 0)
+            x1 = x;
+            f1 = fx;
+        else
+            x0 = x;
+            f0 = fx;
+        end
+        
+        % Aggiornamento del contatore delle iterazioni
+        iter = iter + 1;
     end
-    if(print > 0)
-        fprintf("\n");
-    end
+    
+    % Se l'algoritmo non converge entro il numero massimo di iterazioni consentite,
+    % viene generato un messaggio di errore e restituita l'ultima approssimazione trovata
+    warning('Metodo delle corde: limite di iterazioni raggiunto');
 end
-
+    
